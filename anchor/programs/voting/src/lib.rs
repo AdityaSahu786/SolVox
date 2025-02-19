c69d7cfa6b2bf8f2e24fdba914c1e0b528897b24
AdityaSahu786 <sahuadi786183@gmail.com>
2025-02-20 01:08:05 +0530

    feat: added lib.ts dependency

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -1,14 +1,24 @@
-#![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
-
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod voting {
     use super::*;
 
-    pub fn initialize_poll(ctx: Context:<InitializePoll>, _poll_id: u64) -> ProgramResult {
+    pub fn initialize_poll(ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub poll: Account<'info, Poll>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Poll {
+    pub poll_id: u64,
+    pub votes: u64,
 }
